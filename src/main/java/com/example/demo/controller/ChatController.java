c7beaf1e9bf7ba52d12029b2a6757d4ef765e284
lthung <hunglt31@vinbrain.net>
2024-10-08 01:20:21 +0700

    cors

--- src/main/java/com/example/demo/controller/ChatController.java
+++ src/main/java/com/example/demo/controller/ChatController.java
@@ -1,7 +1,6 @@
 package com.example.demo.controller;
 
 import com.example.demo.service.OpenAIService;
-
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
