390fa4a065972dc6efb89f129cc882f80d8fc2ce
lthung <hunglt31@vinbrain.net>
2024-10-11 17:19:46 +0700

    cors fix

--- src/main/java/com/example/demo/config/WebConfig.java
+++ src/main/java/com/example/demo/config/WebConfig.java
@@ -1,7 +1,6 @@
 package com.example.demo.config;
 
 import org.springframework.context.annotation.Bean;
-import org.springframework.lang.NonNull;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.stereotype.Component;
@@ -13,13 +12,9 @@ public class WebConfig {
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
-            public void addCorsMappings(@NonNull CorsRegistry registry) {
-                registry.addMapping("/api/**") // Match all endpoints under /api, including subpaths
-                        .allowedOrigins("http://localhost:3000/*", "http://localhost:8080") // Specify allowed origins
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allow all necessary HTTP methods,
-                                                                                   // including OPTIONS for preflight
-                        .allowedHeaders("*") // Allow all headers
-                        .allowCredentials(true); // Allow credentials (cookies, authorization headers, etc.)
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/api/**").allowedOrigins("*");
+                registry.addMapping("/api/history/*").allowedOrigins("*");
             }
         };
     }
