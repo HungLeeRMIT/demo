a690c13cd30814a13ea5215e4b84640bb5d980b8
lthung <hunglt31@vinbrain.net>
2024-10-11 23:14:40 +0700

    update delete in cors

--- src/main/java/com/example/demo/controller/ChatController.java
+++ src/main/java/com/example/demo/controller/ChatController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,7 +18,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api")
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "http://localhost:3000", methods = { RequestMethod.DELETE })
 public class ChatController {
 
     private final OpenAIService openAIService;
